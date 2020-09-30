1599c350e23955daac29438aafdb2fce00df369b
Jack Chan <yingyeung575@gmail.com>
2020-09-30 22:42:40 +0800

    23 commit

--- functions/identity-signup.js
+++ functions/identity-signup.js
@@ -10,7 +10,7 @@ exports.handler = async (event) => {
   // subscribe the new customer to the free plan
   await stripe.subscriptions.create({
     customer: customer.id,
-    items: [{ price: process.env.STRIPE_DEFAULT_PRICE_PLAN }],
+  //  items: [{ price: process.env.STRIPE_DEFAULT_PRICE_PLAN }],
   });
 
   // store the Netlify and Stripe IDs in Fauna
