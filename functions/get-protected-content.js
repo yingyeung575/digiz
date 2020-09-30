a5c408bb187925c6f8aaee83e6233a51ca4080f8
Jack Chan <yingyeung575@gmail.com>
2020-09-30 22:32:25 +0800

    22 commit

--- functions/get-protected-content.js
+++ functions/get-protected-content.js
@@ -1,51 +1,37 @@
-const content = {
-    free: {
-      src:
-        'https://images.unsplash.com/photo-1550159930-40066082a4fc?auto=format&fit=crop&w=600&h=600&q=80',
-      alt: 'corgi in the park with a sunset in the background',
-      credit: 'Jacob Van Blarcom',
-      creditLink: 'https://unsplash.com/photos/lkzjENdWgd8',
-      message: 'To view this content, you need to create an account!',
-      allowedRoles: ['free', 'pro', 'premium'],
-    },
-    pro: {
-      src:
-        'https://images.unsplash.com/photo-1519098901909-b1553a1190af?auto=format&fit=crop&w=600&h=600&q=80',
-      alt: 'close-up of a corgi with its tongue hanging out',
-      credit: 'Florencia Potter',
-      creditLink: 'https://unsplash.com/photos/yxmNWxi3wCo',
-      message:
-        'This is protected content! It’s only available if you have a pro plan or higher.',
-      allowedRoles: ['pro', 'premium'],
-    },
-    premium: {
-      src:
-        'https://images.unsplash.com/photo-1546975490-e8b92a360b24?auto=format&fit=crop&w=600&h=600&q=80',
-      alt: 'corgi in a tent with string lights in the foreground',
-      credit: 'Cole Keister',
-      creditLink: 'https://unsplash.com/photos/cX-KEISwDIw',
-      message:
-        'This is protected content! It’s only available if you have the premium plan.',
-      allowedRoles: ['premium'],
-    },
-    jack: {
-        src:
-          'https://images.unsplash.com/photo-1546975490-e8b92a360b24?auto=format&fit=crop&w=600&h=600&q=80',
-        alt: 'corgi in a tent with string lights in the foreground',
-        credit: 'Cole Keister',
-        creditLink: 'https://unsplash.com/photos/cX-KEISwDIw',
-        message:
-          'Jack This is protected content! It’s only available if you have the premium plan.',
-        allowedRoles: ['premium'],
-    },
-  };
-  
-  exports.handler = async (event) => {
+
+    exports.handler = async (event, context) => {
     
-    let temp = JSON.parse(event.body);
+  //  let temp = JSON.parse(event.body);
+
+  const { user } = context.clientContext;
+  console.log(user);
+
+  const roles = user ? user.app_metadata.roles : false;
+  console.log(roles);
+  if (roles[0] == 'admin') {
+    return {
+        statusCode: 200,
+        body: JSON.stringify('admin'),
+    };
+  }
+  else if (roles[0] == 'pro') {
+    return {
+        statusCode: 200,
+        body: JSON.stringify('pro'),
+    };
+  }
+  else if (roles[0] == 'basic') {
+    return {
+        statusCode: 200,
+        body: JSON.stringify('basic'),
+    };
+  }
 
     return {
       statusCode: 200,
-      body: JSON.stringify(temp.type),
+     // body: JSON.stringify(temp.type),
+      body: JSON.stringify('nocontent'),
     };
   };
+
+ 
--- pages/index.vue
+++ pages/index.vue
@@ -4,7 +4,7 @@
     <jheader />
     <h2>Content</h2>
     {{ jcontent }}
-      
+
     
   </div>
 </template>
@@ -13,22 +13,30 @@
 import jheader from '~/components/jheader.vue'
 
 export default {
+
   components: {
     jheader
   },
   data(){
     return{
       jcontent: 'default',
+   
       
     }
   },
   async mounted(){
 
-      let temp = {
-        type: 'jack',
-        age: 18
-      }
-      this.jcontent = await this.$axios.$post('https://digiz.netlify.app/.netlify/functions/get-protected-content',temp) 
+    if (this.$store.state.user.currentUser){
+        let temp = {
+          type: 'jack',
+          age: 18
+        }
+        this.jcontent = await this.$axios.$post('http://localhost:8888/.netlify/functions/get-protected-content',temp,{
+              headers:{
+                      Authorization: 'Bearer '+this.$store.state.user.currentUser.token.access_token
+              }
+        }) 
+    }
   },
 
 }
