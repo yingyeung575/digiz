3c25e5cce5d5bf3d0d29f957230c042e7c006a19
Jack Chan <yingyeung575@gmail.com>
2020-10-01 19:28:29 +0800

    1oct25

--- functions/create-checkout.js
+++ functions/create-checkout.js
@@ -2,16 +2,17 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 
 exports.handler = async (event) => {
-  //const { sku, quantity } = JSON.parse(event.body);
+  const { email } = JSON.parse(event.body);
   // const product = inventory.find((p) => p.sku === sku);
   //const validatedQuantity = quantity > 0 && quantity < 11 ? quantity : 1;
 
   const session = await stripe.checkout.sessions.create({
     payment_method_types: ['card'],
-    billing_address_collection: 'auto',
+  //  billing_address_collection: 'auto',
     success_url: `${process.env.URL}/success`,
     cancel_url: process.env.URL,
     mode: 'subscription',
+    customer_email: email, 
     line_items: [
       {
         price: 'price_1HX6GnFlxrdsMvOD9bUgGlXy',
--- pages/index.vue
+++ pages/index.vue
@@ -9,12 +9,26 @@
     <h2>Test one time payment</h2>
     <button @click='checkout'>Pay</button>
 
+    <h2>Gotrue Register User</h2>
+
+      Name: <input type='text' v-model='name'><br>
+      Email: <input type='email' v-model='email'><br>
+      Password: <input type='password' v-model='password'>
+      <button @click='gsubmit'>Submit</button>
+
     
   </div>
 </template>
 
 <script>
 import jheader from '~/components/jheader.vue'
+import GoTrue from 'gotrue-js';
+
+const auth = new GoTrue({
+  APIUrl: 'https://digiz.netlify.app/.netlify/identity',
+  audience: '',
+  setCookie: false,
+});
 
 export default {
 
@@ -24,7 +38,10 @@ export default {
   data(){
     return{
       jcontent: 'default',
-   
+      name: '',
+      email: '',
+      password: ''
+  
       
     }
   },
@@ -64,7 +81,7 @@ export default {
         
           const r = await this.$axios.$post('https://digiz.netlify.app/.netlify/functions/create-checkout',
           {
-
+            email: this.email
           },{
                 headers:{
                        
@@ -85,7 +102,19 @@ export default {
             }
 
       
+    },
+    async gsubmit(){
+        
+          auth
+          .signup(this.email, this.password)
+          .then((response) => {
+              this.checkout()
+          })
+          .catch((error) => console.log("It's an error", error));
+
+      
     }
+
   }
 
 }
